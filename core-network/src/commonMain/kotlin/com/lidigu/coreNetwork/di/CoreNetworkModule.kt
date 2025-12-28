9fba239b3d1d9474f6f9ea002608f10413d6f695
Manueedes <manuleedes@gmail.com>
2025-12-28 16:38:45 +0300

    setting up koin library

--- core-network/src/commonMain/kotlin/com/lidigu/coreNetwork/di/CoreNetworkModule.kt
+++ core-network/src/commonMain/kotlin/com/lidigu/coreNetwork/di/CoreNetworkModule.kt
@@ -1,2 +1,11 @@
 package com.lidigu.coreNetwork.di
 
+import com.lidigu.coreNetwork.apiService.ApiService
+import com.lidigu.coreNetwork.client.KtorClient
+import org.koin.dsl.module
+
+fun getCoreNetworkModule() = module {
+    single {
+        ApiService(httpClient = KtorClient.getInstance())
+    }
+}
