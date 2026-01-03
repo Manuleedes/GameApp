6873e3080a96abaecac233ba0d110579ca1855a1
Manueedes <manuleedes@gmail.com>
2026-01-03 23:02:14 +0300

    dependency injection for the use cases

---
+++ favorite/domain/src/commonMain/kotlin/com/lidigu/favorite/domain/di/FavoriteDomainModule.kt
@@ -0,0 +1,15 @@
+package com.lidigu.favorite.domain.di
+
+import com.lidigu.favorite.domain.useCases.DeleteUseCase
+import com.lidigu.favorite.domain.useCases.GetAllLocalCasedGamesUseCase
+import com.lidigu.favorite.domain.useCases.UpsertUseCase
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+fun getFavoriteDomainModule(): Module {
+    return module {
+        factory { DeleteUseCase(get()) }
+        factory { GetAllLocalCasedGamesUseCase(get()) }
+        factory { UpsertUseCase(get()) }
+    }
+}
